0c25d308f79a8bde7870b34001a62fd8750acacf
Arek Moszczynski <arekmoszczynski@gmail.com>
2020-10-04 20:53:30 +0200

    refactor(components/Link.js): add hook within component

--- examples/todomvc/src/components/Link.js
+++ examples/todomvc/src/components/Link.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useCallback, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
-const Link = ({ active, children, setFilter }) =>
-  (
-    // eslint-disable-next-line jsx-a11y/anchor-is-valid
-    <a
-      className={classnames({ selected: active })}
-      style={{ cursor: 'pointer' }}
-      onClick={() => setFilter()}
-    >
-      {children}
-    </a>
-  )
+const Link = ({ active, children, setFilter }) => {
+  // eslint-disable-next-line jsx-a11y/anchor-is-valid
+  const className = useMemo(() => classnames({ selected: active }), [active]);
+  const handleClick = useCallback(() => setFilter(), [setFilter]);
+  const styleObject = useMemo(() => ({ cursor: 'pointer' }), []);
 
+  return <a
+    className={className}
+    style={styleObject}
+    onClick={handleClick}
+  >
+    {children}
+  </a>
+}
 
 Link.propTypes = {
   active: PropTypes.bool.isRequired,
@@ -21,4 +23,4 @@ Link.propTypes = {
   setFilter: PropTypes.func.isRequired
 }
 
-export default Link
+export default Link;
